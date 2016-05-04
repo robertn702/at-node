72d4fa995a828d1a6e766b31cbaed76a1656ea35
Robert Niimi <robertn702@gmail.com>
2016-05-03 21:48:56 -0700

    adds gitignore and readme

---
+++ src/node-at.js
@@ -0,0 +1,27 @@
+const at = (time, cb) => {
+  let later;
+  if (!time instanceof Date) {
+    let timestamp = Date.parse(time);
+    if (!isNaN(timestamp)) {
+      later = new Date(time);
+    } else {
+      throw new Error('[at] "time" must be parseable date/time');
+    }
+  } else {
+    later = time;
+  }
+
+  const now = new Date();
+  const timeDiff = later.getTime() - now.getTime();
+
+  if (timeDiff <= 0) {
+    cb();
+    return;
+  }
+  const timeout = setTimeout(cb, timeDiff);
+  return () => {
+    clearTimeout(timeout);
+  };
+};
+
+module.exports = at;
